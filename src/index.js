30961f7a46ed68e12e6e068ca1e05f47950bc29f
Joseph Mutua <Mutuaj793@gmail.com>
2020-04-17 22:37:26 +0300

    Add file-loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
  import _ from 'lodash';
  import './style.css';
+ import Icon from './icon.png';
+
+
 
   function component() {
     const element = document.createElement('div');
@@ -7,6 +10,11 @@
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
+    // Add the image to our existing div.
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    
     return element;
   }
 
