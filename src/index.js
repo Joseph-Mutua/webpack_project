94ebff74f3ec7d4ec55d11f3bc996f3b0462944b
Joseph Mutua <Mutuaj793@gmail.com>
2020-04-17 22:57:55 +0300

    Add data-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
  import _ from 'lodash';
  import './style.css';
  import Icon from './icon.png';
+ import Data from './data.xml';
 
 
 
@@ -14,6 +15,8 @@
     const myIcon = new Image();
     myIcon.src = Icon;
     element.appendChild(myIcon);
+
+    console.log(Data);
     
     return element;
   }
